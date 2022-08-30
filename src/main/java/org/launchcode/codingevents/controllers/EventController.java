8074d5fb271e0c8067be0204cd9fbdb90c77a6e1
Chris Bay <chris@chrisbay.net>
2022-08-30 13:18:27 -0500

    Code from ch13 lesson

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +14,34 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static final List<String> events = new ArrayList<>();
+
+    // handles GET requests at /events
+    // the root controller method for this class
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
         return "events/index";
+    }
+
+    // responds to GET requests at /events/create
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create New Event");
+        return "events/create";
+    }
+
+    // responds to POST requests at /events/create
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String newEvent) {
+
+        // "store" the new event
+        events.add(newEvent);
+
+        // send them back to the main event listing
+        // equivalent of sending 302 HTTP response code with Location=/events
+        return "redirect:";
     }
 
 }
